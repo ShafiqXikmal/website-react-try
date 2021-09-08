be6d492691128a6df07f738857011e3a81493cf0
shafiqXikmal <mohdshafiqikmalmuh@gmail.com>
2021-09-08 20:20:45 +0800

    Update and fixing css on home page

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -12,7 +12,7 @@ function NavBar() {
   };
 
   return (
-    <div className="navBar">
+    <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} />
         <div className="hiddenLinks">
@@ -22,14 +22,15 @@ function NavBar() {
           <Link to="/contact">Contact</Link>
         </div>
       </div>
-      <div className="rightSide"></div>
-      <Link to="/">Home</Link>
-      <Link to="/menu">Menu</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-      <button>
-        <ReorderIcon />
-      </button>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <button onClick={toggleNavBar}>
+          <ReorderIcon />
+        </button>
+      </div>
     </div>
   );
 }
