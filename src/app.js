d8ffd2e9e9198dea23de917cdcc97d8b91274c25
shafiqXikmal <mohdshafiqikmalmuh@gmail.com>
2021-09-08 12:41:25 +0800

    update about us files

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import NavBar from "./components/navBar";
 import Footer from "./components/footer";
 import Home from "./pages/home";
 import Menu from "./pages/menu";
+import About from "./pages/about";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home}></Route>
           <Route path="/menu" exact component={Menu}></Route>
+          <Route path="/about" exact component={About}></Route>
         </Switch>
         <Footer />
       </Router>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,32 @@
 import React from "react";
+import multipleLogo from "../assets/crewLogo.jpg";
+import "../styles/about.css";
 
 function About() {
-  return <div></div>;
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${multipleLogo})` }}
+      ></div>
+      <div className="aboutBottom"></div>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+        fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+        accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+        molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+        officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+        nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+        error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis
+        modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias
+        error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt
+        dolorem! Officiis iure rerum voluptates a cumque velit
+      </p>
+    </div>
+  );
 }
 
 module.exports = About;
